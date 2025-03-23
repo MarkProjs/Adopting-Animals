f71ceb17ccef901fe5f5bc5d49ec061e02489ce9
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2025-03-23 15:21:18 -0400

    adding the pets css and js

---
+++ scripts/pets.js
@@ -0,0 +1,32 @@
+const petLists = [
+  {
+    image: "../assets",
+    type: "dog",
+    breed: "Golden Retriever",
+    age: "Young",
+    gender: "Female",
+    getAlong: {
+      otherDogs: "Yes",
+      otherCats: "No",
+      smallChildren: "Yes"
+    },
+    description: "This is a dog",
+    ownerName: "John Doe",
+    ownerEmail: "5V4t8@example.com",
+  }, 
+  {
+    image: "../assets",
+    type: "cat",
+    breed: "Persian",
+    age: "Adult",
+    gender: "Male",
+    getAlong: {
+      otherDogs: "No",
+      otherCats: "Yes",
+      smallChildren: "Yes"
+    },
+    description: "This is a cat",
+    ownerName: "Jane Doe",
+    ownerEmail: "lW1x4@example.com",
+  }
+];
