7c0bdd4ddc1af4fdf9016ca61c0942749354143a
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2025-03-23 16:29:08 -0400

    adding the browse pets

--- scripts/pets.js
+++ scripts/pets.js
@@ -1,6 +1,6 @@
 const petLists = [
   {
-    image: "../assets",
+    image: "../../assets/goldenRetriever.jpg",
     type: "dog",
     breed: "Golden Retriever",
     age: "Young",
@@ -15,7 +15,7 @@ const petLists = [
     ownerEmail: "5V4t8@example.com",
   }, 
   {
-    image: "../assets",
+    image: "../../assets/persianCat.jpg",
     type: "cat",
     breed: "Persian",
     age: "Adult",
@@ -30,3 +30,35 @@ const petLists = [
     ownerEmail: "lW1x4@example.com",
   }
 ];
+
+function addPet(e) {
+  const petList = document.getElementById("petList");
+  const pet = document.createElement("div");
+  pet.classList.add("pet-card");
+
+  pet.innerHTML = `
+    <img src = "${e.image}" alt="${e.type} class="pet-image"/>
+    <div class="pet-info">
+    <h2>${e.breed} ${e.type}</h2>
+      <p><strong>Age: </strong> ${e.age}</p>
+      <p><strong>Gender:</strong> ${e.gender}</p>
+      <p><strong>Get along with:</strong></p>
+      <ul>
+        <li>Other dogs: ${e.getAlong.otherDogs}</li>
+        <li>Other cats: ${e.getAlong.otherCats}</li>
+        <li>Small children: ${e.getAlong.smallChildren}</li>
+      </ul>
+      <p><strong>Description:</strong> ${e.description}</p>
+      <p><strong>Owner Name:</strong> ${e.ownerName}</p>
+      <p><strong>Owner Email:</strong> ${e.ownerEmail}</p>
+    </div>
+  `;
+
+  petList.appendChild(pet);
+}
+
+function loadPets() {
+  petLists.forEach(addPet);
+}
+
+loadPets();
